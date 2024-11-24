7715e2347a7db89890960b201b9fcd160d1b9770
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-11-24 17:53:22 +0530

    moved recipes to mongo

--- lib/cooking_page.dart
+++ lib/cooking_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'package:ChefMate/recipe.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'recipes.dart';
 
 class CookingPage extends StatefulWidget {
   final Recipe recipe;
@@ -25,7 +25,7 @@ class _CookingPageState extends State<CookingPage> {
 
   void _startTimer(int minutes) {
     setState(() {
-      _timerValue = minutes * 60 * widget.personCount;
+      _timerValue = minutes * 60; // Timer value is in seconds
       _isPaused = false;
       _progress = 1.0; // Reset progress to full circle
     });
@@ -34,8 +34,7 @@ class _CookingPageState extends State<CookingPage> {
       if (_timerValue > 0 && !_isPaused) {
         setState(() {
           _timerValue--;
-          _progress = _timerValue /
-              (minutes * 60 * widget.personCount); // Update progress
+          _progress = _timerValue / (minutes * 60); // Update progress
         });
       } else if (_timerValue == 0) {
         timer.cancel();
@@ -60,7 +59,8 @@ class _CookingPageState extends State<CookingPage> {
           _currentStep++;
           _isStepCompleted = false; // Reset step completed flag
         });
-        _startTimer(widget.recipe.steps[_currentStep].time);
+        // Start the timer for the next step
+        _startTimer(widget.recipe.steps[_currentStep][1]); // Get time from the next step
       } else {
         _showCompletionScreen();
       }
@@ -121,7 +121,8 @@ class _CookingPageState extends State<CookingPage> {
   @override
   void initState() {
     super.initState();
-    _startTimer(widget.recipe.steps[_currentStep].time);
+    // Start the timer for the first step
+    _startTimer(widget.recipe.steps[_currentStep][1]); // Get time from first step
   }
 
   @override
@@ -134,10 +135,11 @@ class _CookingPageState extends State<CookingPage> {
   @override
   Widget build(BuildContext context) {
     final currentStep = widget.recipe.steps[_currentStep];
+    String description = currentStep[0]; // Accessing the description
+    int time = currentStep[1]; // Accessing the time (in minutes)
 
     return Scaffold(
-      backgroundColor:
-          Colors.transparent, // Make the Scaffold background transparent
+      backgroundColor: Colors.transparent, // Make the Scaffold background transparent
       appBar: AppBar(
         title: Text('Cooking ${widget.recipe.name}',
             style: const TextStyle(color: Color(0xFF229799))),
@@ -159,7 +161,7 @@ class _CookingPageState extends State<CookingPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
-                currentStep.description,
+                description, // Use the description here
                 style: const TextStyle(
                     fontSize: 24, color: Color.fromARGB(255, 255, 255, 255)),
                 textAlign: TextAlign.center,
@@ -168,8 +170,7 @@ class _CookingPageState extends State<CookingPage> {
               LayoutBuilder(
                 builder: (context, constraints) {
                   // Calculate the size for the circle to be equal in width and height
-                  double circleSize = constraints.maxWidth *
-                      0.7; // Make it 70% of the screen width
+                  double circleSize = constraints.maxWidth * 0.7; // Make it 70% of the screen width
                   return Center(
                     child: Stack(
                       alignment: Alignment.center,
@@ -181,10 +182,8 @@ class _CookingPageState extends State<CookingPage> {
                           child: CircularProgressIndicator(
                             value: _progress,
                             strokeWidth: 15,
-                            backgroundColor:
-                                const Color.fromARGB(255, 88, 73, 73),
-                            valueColor: const AlwaysStoppedAnimation<Color>(
-                                Color(0xFF48CFCB)),
+                            backgroundColor: const Color.fromARGB(255, 88, 73, 73),
+                            valueColor: const AlwaysStoppedAnimation<Color>(Color(0xFF48CFCB)),
                           ),
                         ),
                         Text(
--- lib/recipe_listing_page.dart
+++ lib/recipe_listing_page.dart
@@ -1,49 +1,84 @@
+import 'package:ChefMate/cooking_page.dart';
 import 'package:flutter/material.dart';
-import 'recipes.dart';
-import 'cooking_page.dart';
+import 'mongo_helper.dart';  // MongoDB helper class
+import 'recipe.dart';  // Recipe and Ingredient models
 
-class RecipeListingPage extends StatelessWidget {
+class RecipeListingPage extends StatefulWidget {
   const RecipeListingPage({super.key});
+
+  @override
+  _RecipeListingPageState createState() => _RecipeListingPageState();
+}
+
+class _RecipeListingPageState extends State<RecipeListingPage> {
+  late Future<List<Recipe>> _recipesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _recipesFuture = MongoHelper.fetchRecipes();  // Fetch recipes from MongoDB
+  }
+
+  @override
+  void dispose() {
+    MongoHelper.close();  // Close the MongoDB connection when the page is disposed
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true, // Allow body to extend behind the AppBar
-      backgroundColor: Colors.transparent, // Make the background color transparent to show the image
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: const Text('Recipes', style: TextStyle(color: Color(0xFF229799))),
-        backgroundColor: Colors.transparent, // Make the AppBar background transparent
-        elevation: 0, // Remove the shadow from the AppBar
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         centerTitle: true,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/list_bg.jpg'), // Set the background image
-            fit: BoxFit.cover, // Make the image cover the whole screen
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ListView.builder(
-            itemCount: recipes.length,
-            itemBuilder: (context, index) {
-              return Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                child: ListTile(
-                  leading: const Icon(Icons.fastfood, color: Color(0xFF48CFCB)),
-                  title: Text(
-                    recipes[index].name,
-                    style: const TextStyle(color: Color.fromARGB(255, 255, 255, 255), fontSize: 18),
-                  ),
-                  onTap: () {
-                    _showPersonDialog(context, recipes[index]);
+      body: FutureBuilder<List<Recipe>>(
+        future: _recipesFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No recipes found.'));
+          } else {
+            final recipes = snapshot.data!;
+
+            return Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/list_bg.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: recipes.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                      child: ListTile(
+                        leading: const Icon(Icons.fastfood, color: Color(0xFF48CFCB)),
+                        title: Text(
+                          recipes[index].name,
+                          style: const TextStyle(color: Color.fromARGB(255, 255, 255, 255), fontSize: 18),
+                        ),
+                        onTap: () {
+                          _showPersonDialog(context, recipes[index]);
+                        },
+                      ),
+                    );
                   },
                 ),
-              );
-            },
-          ),
-        ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
@@ -69,10 +104,8 @@ class RecipeListingPage extends StatelessWidget {
           actions: [
             TextButton(
               onPressed: () {
-                // Validate input to ensure it's a positive integer
                 String input = personController.text.trim();
                 if (input.isEmpty || int.tryParse(input) == null || int.parse(input) <= 0) {
-                  // Show an error message if the input is invalid
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(content: Text('Please enter a valid number of persons')),
                   );
